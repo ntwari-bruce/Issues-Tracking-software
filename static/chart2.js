37973bfb6d471c778bb5e713d4a9b56e4a665e1c
ntwari bruce <ntwaribruce647@gmail.com>
2023-06-27 20:33:27 +0200

    added charts

---
+++ static/chart2.js
@@ -0,0 +1,21 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work', 8],
+    ['Eat', 2],
+    ['TV', 4],
+    ['Gym', 2],
+    ['Sleep', 8]
+  ]);
+
+  var options = {
+    'width': '100%',
+    'height': '100%'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('chart2'));
+  chart.draw(data, options);
+}
